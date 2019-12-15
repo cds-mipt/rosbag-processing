78f6347caef714b4206fcc736a46d1423a3e6cc6
Dmitry Yudin <yuddim@yandex.ru>
2019-12-15 19:39:34 +0300

    Command line usage update

--- raw2color.py
+++ raw2color.py
@@ -2,37 +2,44 @@ import glob
 import os
 import tqdm
 import cv2
-
-
-path = 'E:/Datasets/NKBVS/Taganrog/2019-08-16-11-24-24_0_a/vis/left/image_raw/data'
-
-path_expression = path + '/*.png'
-imgs = glob.glob(path_expression, recursive=True)
-
-print(imgs[0])
-
-converted_imgs = []
-for im in imgs:
-    converted_imgs.append(im.replace(r'image_raw/data', r'image_raw/converted'))
-print(converted_imgs[0])
+import argparse
 
 def getImage(im_path):
     if os.path.isfile(im_path):
         img = cv2.imread(im_path, cv2.IMREAD_UNCHANGED)
-        img = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2BGR)
-        #img = cv2.cvtColor(img, cv2.COLOR_BAYER_GB2RGB)
+        #img = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2BGR)
+        img = cv2.cvtColor(img, cv2.COLOR_BAYER_GB2RGB)
         #img = img[:, :, [2, 1, 0]]
         return img
     else:
         return None
 
+def main_raw2color(path):
+    if path is None:
+        path = 'E:/Datasets/NKBVS/Taganrog/2019-08-16-11-24-24_0_a/vis/left/image_raw/data'
 
-for i in tqdm.tqdm(range(len(converted_imgs))):
-    if not os.path.exists(os.path.dirname(converted_imgs[i])):
-        os.makedirs(os.path.dirname(converted_imgs[i]))
-    if not os.path.exists(converted_imgs[i]):
-        try:
-            color_image = getImage(imgs[i])
-            cv2.imwrite(converted_imgs[i], color_image)
-        except:
-            print(imgs[i])
+    path_expression = path + '/*.png'
+    imgs = glob.glob(path_expression)
+
+    print('Path processing is started..')
+
+    converted_imgs = []
+    for im in imgs:
+        converted_imgs.append(im.replace(r'image_raw/data', r'image_raw/converted'))
+    #print(converted_imgs[0])
+
+    for i in tqdm.tqdm(range(len(converted_imgs))):
+        if not os.path.exists(os.path.dirname(converted_imgs[i])):
+            os.makedirs(os.path.dirname(converted_imgs[i]))
+        if not os.path.exists(converted_imgs[i]):
+            try:
+                color_image = getImage(imgs[i])
+                cv2.imwrite(converted_imgs[i], color_image)
+            except:
+                print(imgs[i])
+
+if __name__ == '__main__':
+	argparser = argparse.ArgumentParser(description='Convert image form Bayer to RGB')
+	argparser.add_argument('path', help='path to bag file')
+	args = argparser.parse_args()
+	main_raw2color(**vars(args))
